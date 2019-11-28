2d5f15b65b820eae2df489c9668e67159befb0f3
Jakub Nozderka <jaktotowiem@o2.pl>
2019-11-28 17:07:31 +0100

    Loading data from file, not properly in 100% but loading

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from dataStructure import DataStructure
+
+data = np.loadtxt('data/ecoli.txt', dtype=bytes) 
+
+print(data)
+
+array = []
+
+# for i in range(len(data)):
+    # array.append(DataStructure(data[i]))    
+
---
+++ dataStructure.py
@@ -0,0 +1,14 @@
+
+
+class DataStructure:
+
+    def __init__(self, sequenceName, mcg, gvh, lip, chg, aac, alm1, alm2, localization):
+        self.sequenceName = sequenceName 
+        self.mcg = mcg
+        self.gvh = gvh
+        self.lip = lip
+        self.chg = chg
+        self.aac = aac
+        self.alm1 = alm1
+        self.alm2 = alm2
+        self.localization = localization
