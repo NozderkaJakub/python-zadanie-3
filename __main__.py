7fdac4f8cad21f3ba6fb8b0f968f930e21fa9c64
mic216 <michal.wlodarczyk85@wp.pl>
2019-12-02 18:23:39 +0100

    median, minimum, maximum, arithmetic mean calculation

--- __main__.py
+++ __main__.py
@@ -8,4 +8,11 @@ data = np.loadtxt('./data/ecoli.txt', dtype={'names': (
         'U20', np.float, np.float, np.float, np.float, np.float, np.float, np.float, "U3"
 )})
 
-print(data)
+dataStructure = DataStructure(data['sequenceName'], data['mcg'], data['gvh'], data['lip'],
+                              data['chg'], data['aac'], data['alm1'], data['alm2'], data['localization'])
+
+print(dataStructure.gvh)
+print(DataStructure.calc_arithmetic_mean(dataStructure.gvh))
+print(DataStructure.calc_median(dataStructure.gvh))
+print(DataStructure.calc_minimum(dataStructure.gvh))
+print(DataStructure.calc_maximum(dataStructure.gvh))
--- data_structure.py
+++ data_structure.py
@@ -1,7 +1,11 @@
+import numpy as np
+import math
+
+
 class DataStructure:
 
     def __init__(self, sequenceName, mcg, gvh, lip, chg, aac, alm1, alm2, localization):
-        self.sequenceName = sequenceName 
+        self.sequenceName = sequenceName
         self.mcg = mcg
         self.gvh = gvh
         self.lip = lip
@@ -10,3 +14,25 @@ class DataStructure:
         self.alm1 = alm1
         self.alm2 = alm2
         self.localization = localization
+
+    @classmethod
+    def calc_arithmetic_mean(cls, data):
+        return np.sum(data)/len(data)
+
+    @classmethod
+    def calc_standard_deviation(cls, data):
+        pass
+
+    @classmethod
+    def calc_median(cls, data):
+        length = len(data)
+        data = np.sort(data)
+        return data[(length-1)//2] if length % 2 != 0 else (data[(length//2)-1] + data[length//2]) / 2
+
+    @classmethod
+    def calc_minimum(cls, data):
+        return min(data)
+
+    @classmethod
+    def calc_maximum(cls, data):
+        return max(data)
